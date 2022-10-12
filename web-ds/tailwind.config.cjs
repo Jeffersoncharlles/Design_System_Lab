819cf7b52678a6564f2caabea053e56ad5023794
Jefferson charlles <contato@jefferdeveloper.com>
2022-10-12 00:18:45 -0300

    chore:start config tokens

--- web-ds/tailwind.config.cjs
+++ web-ds/tailwind.config.cjs
@@ -4,7 +4,21 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
-    extend: {},
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-500': '#7C7C7A',
+      'gray-400': '#7C7C8A',
+      'gray-200': '#C4C4CC',
+      'gray-100': '#E1E1E6',
+      'cyan-500': '#81D8F7',
+      'gray-300': '#BDEEFF',
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    },
   },
   plugins: [],
 }
