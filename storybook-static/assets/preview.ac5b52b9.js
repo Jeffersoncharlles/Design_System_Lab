b3621721804ae273e05645832cf0bf8a5c3587b6
Jefferson charlles <contato@jefferdeveloper.com>
2022-10-13 01:14:30 -0300

    chore:deploy ci workflow yml replace

---
+++ storybook-static/assets/preview.ac5b52b9.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.5ea108f0.js";import"./iframe.498e99e3.js";import"./es.map.constructor.8919b857.js";import"./es.number.to-fixed.fcfe13d6.js";import"./index.1e9b7510.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.ac5b52b9.js.map
